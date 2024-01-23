bde01864fe08881fd3a21a64c67c3bc6bf97a0f6
ketchup0211 <dlcka1111@naver.com>
2024-01-23 09:33:48 +0900

    [MOD] toggleDone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import { useState, useEffect } from "react";
-import ToDoCard from "./toDoCard.js";
+//import ToDoCard from "./toDoCard.js";
 
 function App() {
   const [title, setTitle] = useState("");
   const [toDo, setToDo] = useState("");
+  const [done, setDone] = useState(false);
   const [toDos, setToDos] = useState([]);
   const onTitleChange = (event) => setTitle(event.target.value);
   const onToDoChange = (event) => setToDo(event.target.value);
+
+  function ToDoCard({ title, toDo, done }) {
+    return (
+      <div>
+        <h3>{title}</h3>
+        <p>{toDo}</p>
+        <span>
+          <DeleteBtn />
+          <DoneBtn text={done ? "취소" : "완료"} />
+        </span>
+      </div>
+    );
+  }
+
+  function DeleteBtn() {
+    return <button>삭제하기</button>;
+  }
+
+  function DoneBtn({ text }) {
+    const onClick = () => setDone((current) => !current);
+    return <button onClick={onClick}>{text}</button>;
+  }
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -15,8 +38,10 @@ function App() {
     }
     setTitle("");
     setToDo("");
-    setToDos((currentArray) => [[title, toDo], ...currentArray]);
+    setToDos((currentArray) => [[title, toDo, done], ...currentArray]);
   };
+
+  const toggleDone = (idx) => {};
   return (
     <div>
       <h1>My Todo List ({toDos.length})</h1>
@@ -41,8 +66,8 @@ function App() {
       </form>
       <hr />
       <h2>Working..🔥</h2>
-      {toDos.map(([title, toDo], idx) => (
-        <ToDoCard title={title} toDo={toDo} />
+      {toDos.map(([title, toDo, done]) => (
+        <ToDoCard title={title} toDo={toDo} done={done} />
       ))}
       <h2>Done..!🎉</h2>
     </div>
