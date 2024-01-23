1b1c16f42c2cbf4cbf485f11eb798ee84bd4642e
ketchup0211 <dlcka1111@naver.com>
2024-01-23 09:12:06 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import ToDoCard from "./toDoCard.js";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onTitleChange = (event) => setTitle(event.target.value);
+  const onToDoChange = (event) => setToDo(event.target.value);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (title === "" || toDo === "") {
+      return;
+    }
+    setTitle("");
+    setToDo("");
+    setToDos((currentArray) => [[title, toDo], ...currentArray]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Todo List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="title">제목</label>
+        <input
+          id="title"
+          onChange={onTitleChange}
+          value={title}
+          type="text"
+          placeholder="title"
+        ></input>
+        <label htmlFor="title">내용</label>
+        <input
+          id="toDo"
+          onChange={onToDoChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to-do..."
+        ></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <h2>Working..🔥</h2>
+      {toDos.map(([title, toDo], idx) => (
+        <ToDoCard title={title} toDo={toDo} />
+      ))}
+      <h2>Done..!🎉</h2>
     </div>
   );
 }
